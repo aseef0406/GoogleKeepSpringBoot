30fefadf3c441cb2b392ee066d60740c822f5cd9
aseef0406 <mohammed.aseef04@gmail.com>
2022-05-06 01:04:09 +0530

    Starting Project First Commit

---
+++ src/main/java/com/keep/googlekeepspringboot/GoogleKeepSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.keep.googlekeepspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleKeepSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleKeepSpringBootApplication.class, args);
+	}
+
+}

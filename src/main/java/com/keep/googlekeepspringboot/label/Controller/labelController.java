239d7b5e0194e388c460ca47103fbeb080d9c8dd
Sandeep Singh <sandeepgaurdec13@gmail.com>
2022-05-09 19:35:31 +0530

    label Controller

---
+++ src/main/java/com/keep/googlekeepspringboot/label/Controller/labelController.java
@@ -0,0 +1,5 @@
+package com.keep.googlekeepspringboot.label.Controller;
+
+public class labelController {
+
+}
--- src/main/java/com/keep/googlekeepspringboot/label/Entity/label.java
+++ src/main/java/com/keep/googlekeepspringboot/label/Entity/label.java
@@ -1,5 +1,54 @@
 package com.keep.googlekeepspringboot.label.Entity;
 
 public class label {
+	private int labelId;
+	private String label;
+	private long userId;
+	public int noteId;
+
+	label() {
+	}
+
+	label(String label) {
+		this.label = label;
+
+	}
+
+	public int getLabelId() {
+		return labelId;
+	}
+
+	public void setLabelId(int labelId) {
+		this.labelId = labelId;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public int getNoteId() {
+		return noteId;
+	}
+
+	public void setNoteId(int noteId) {
+		this.noteId = noteId;
+	}
+
+	@Override
+	public String toString() {
+		return "label [labelId=" + labelId + ", label=" + label + ", userId=" + userId + ", noteId=" + noteId + "]";
+	}
 
 }
